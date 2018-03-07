43ec97de267314c035c16df16951f4ade33a7271
artum <jacopo.libe@gmail.com>
2018-03-07 16:58:45 +0100

    Creazione progetto JavaFX

---
+++ Source/src/com/childcare/client/Main.java
@@ -0,0 +1,22 @@
+package com.childcare.client;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        //primaryStage.setTitle("Hello World");
+        //primaryStage.setScene(new Scene(root, 300, 275));
+        //primaryStage.show();
+        Platform.exit();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
