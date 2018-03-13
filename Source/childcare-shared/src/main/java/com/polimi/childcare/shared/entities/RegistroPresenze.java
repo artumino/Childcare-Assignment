bedac916dea8fa7e5789d37bfee71d993ed88f2c
artum <jacopo.libe@gmail.com>
2018-03-13 10:37:35 +0100

    Fix registro presenze per usare @Enumerated e @Temportal

--- Source/childcare-shared/src/main/java/com/polimi/childcare/shared/entities/RegistroPresenze.java
+++ Source/childcare-shared/src/main/java/com/polimi/childcare/shared/entities/RegistroPresenze.java
@@ -21,14 +21,17 @@ public class RegistroPresenze implements Serializable
     private Persona Persona;
 
     @DatabaseField(dataType = DataType.ENUM_INTEGER, canBeNull = false) //TODO: Enum in hibernate
+    @Enumerated(EnumType.ORDINAL)
     private StatoPresenza Stato;
 
     @DatabaseField(dataType = DataType.DATE, canBeNull = false)
     @Column(nullable = false)
+    @Temporal(value = TemporalType.DATE)
     private Date Date;
 
     @DatabaseField(dataType = DataType.DATE_TIME, canBeNull = false)
     @Column(nullable = false)
+    @Temporal(value = TemporalType.TIMESTAMP)
     private Date TimeStamp;
 
     @DatabaseField(dataType = DataType.SHORT, canBeNull = false)
