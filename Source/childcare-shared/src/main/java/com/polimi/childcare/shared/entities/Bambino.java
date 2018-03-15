d1b5e7f377c385f84e1a1877a0b03e075c32f516
artum <jacopo.libe@gmail.com>
2018-03-15 16:05:40 +0100

    Aggiunta relazione ManyToMany di esempio

--- Source/childcare-shared/src/main/java/com/polimi/childcare/shared/entities/Bambino.java
+++ Source/childcare-shared/src/main/java/com/polimi/childcare/shared/entities/Bambino.java
@@ -2,10 +2,13 @@ package com.polimi.childcare.shared.entities;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import java.util.Set;
 
 @Entity
 @DiscriminatorValue(value = "0")
 public class Bambino extends Persona
 {
-
+    @ManyToMany(mappedBy = "bambini")
+    private Set<Gruppo> gruppi;
 }
--- Source/childcare-shared/src/main/java/com/polimi/childcare/shared/entities/Gruppo.java
+++ Source/childcare-shared/src/main/java/com/polimi/childcare/shared/entities/Gruppo.java
@@ -3,6 +3,9 @@ import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 @DatabaseTable(tableName = "Gruppo")
 @Entity
@@ -13,4 +16,15 @@ public class Gruppo implements Serializable
     @Id
     private Long ID;
     //Qua completeremo con ID Tutore e ID Bambino esterni
+
+    @OneToOne(optional = false, cascade = CascadeType.REFRESH)
+    private Addetto sorvergliante;
+
+    @ManyToMany(cascade = { CascadeType.ALL }, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "Gruppo_Bambino",
+            joinColumns = { @JoinColumn(name = "gruppo_id") },
+            inverseJoinColumns = { @JoinColumn(name = "bambino_id") }
+    )
+    private Set<Bambino> bambini;
 }
