bb4ab7a312c8437addd7056d6ca9b93e66bfaedc
Unknown <ash96@hotmail.it>
2018-05-15 21:24:05 +0200

    Limit e Skip
    Aggiunti limit e skip gestiti con annessa bad request

--- Source/childcare-server/src/main/java/com/polimi/childcare/server/handlers/BambiniRequestHandler.java
+++ Source/childcare-server/src/main/java/com/polimi/childcare/server/handlers/BambiniRequestHandler.java
@@ -4,6 +4,7 @@ import com.polimi.childcare.server.database.DatabaseSession;
 import com.polimi.childcare.server.networking.IRequestHandler;
 import com.polimi.childcare.shared.entities.Bambino;
 import com.polimi.childcare.shared.networking.requests.BambiniRequest;
+import com.polimi.childcare.shared.networking.responses.BadRequestResponse;
 import com.polimi.childcare.shared.networking.responses.BaseResponse;
 import com.polimi.childcare.shared.networking.responses.lists.ListBambiniResponse;
 
@@ -15,11 +16,19 @@ public class BambiniRequestHandler implements IRequestHandler<BambiniRequest>
     @Override
     public BaseResponse processRequest(BambiniRequest request)
     {
+        if(request.getCount() < 0 || request.getPageNumber() < 0)
+            return new BadRequestResponse();
         List<Bambino> bambini = new ArrayList<>();
-        DatabaseSession.getInstance().execute(session -> {
-            bambini.addAll(session.query(Bambino.class).toList());
-            return true;
-        });
+        if(request.getCount() == 0)
+            DatabaseSession.getInstance().execute(session -> {
+                bambini.addAll(session.query(Bambino.class).toList());
+                return true;
+            });
+        else
+            DatabaseSession.getInstance().execute(session -> {
+                bambini.addAll(session.query(Bambino.class).limit(request.getCount()*(request.getPageNumber() + 1)).skip(request.getCount()*request.getPageNumber()).toList());
+                return true;
+            });
 
         ListBambiniResponse risposta = new ListBambiniResponse(200, bambini);
 
