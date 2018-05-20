329cda9c62441f2e72b68ac53b7916b6d120b0a1
artum <jacopo.libe@gmail.com>
2018-05-21 00:11:52 +0200

    Fix dal merge

--- Source/childcare-shared/src/test/java/NetworkManagerTest.java
+++ Source/childcare-shared/src/test/java/NetworkManagerTest.java
@@ -1,4 +1,5 @@
 import com.polimi.childcare.client.shared.networking.IClientNetworkInterface;
+import com.polimi.childcare.client.shared.networking.exceptions.NetworkSerializationException;
 import com.polimi.childcare.shared.entities.Bambino;
 import com.polimi.childcare.shared.networking.requests.NullRequest;
 import com.polimi.childcare.shared.networking.responses.BaseResponse;
@@ -21,7 +22,12 @@ public class NetworkManagerTest
     {
         for(int i = 0; i < 50; i++)
         {
-            Assert.assertNotNull("Ritornata risposta errata", netRule.createDummyClient(i % 2 == 0 ? NetworkingRule.TestType.Sockets : NetworkingRule.TestType.RMI).sendMessage(new NullRequest()));
+            try {
+                Assert.assertNotNull("Ritornata risposta errata", netRule.createDummyClient(i % 2 == 0 ? NetworkingRule.TestType.Sockets : NetworkingRule.TestType.RMI).sendMessage(new NullRequest()));
+            } catch (NetworkSerializationException e) {
+                e.printStackTrace();
+                Assert.fail("Send message errore di serializzazione!");
+            }
         }
     }
 
@@ -34,7 +40,12 @@ public class NetworkManagerTest
             ArrayList<Bambino> bambini = NetworkTestStub.createBambiniList();
             IClientNetworkInterface client = netRule.createDummyClient(i % 2 == 0 ? NetworkingRule.TestType.Sockets : NetworkingRule.TestType.RMI);
 
-            BaseResponse baseResponse = client.sendMessage(new BambinoListRequestStub(123, bambini));
+            BaseResponse baseResponse = null;
+            try {
+                baseResponse = client.sendMessage(new BambinoListRequestStub(123, bambini));
+            } catch (NetworkSerializationException e) {
+                e.printStackTrace();
+            }
             NetworkTestStub.TestMultipleClientsPayloadedAsserts(bambini, baseResponse);
         }
     }
