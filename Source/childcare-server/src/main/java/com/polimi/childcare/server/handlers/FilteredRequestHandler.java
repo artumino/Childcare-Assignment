8749b5356a88d5087b319384cc05ed54f5eef7f1
artum <jacopo.libe@gmail.com>
2018-05-23 03:59:58 +0200

    Aggiunta dipendenza mancante

--- Source/childcare-server/src/main/java/com/polimi/childcare/server/handlers/FilteredRequestHandler.java
+++ Source/childcare-server/src/main/java/com/polimi/childcare/server/handlers/FilteredRequestHandler.java
@@ -1,5 +1,6 @@
 package com.polimi.childcare.server.handlers;
 
+import com.polimi.childcare.shared.dto.DTOUtils;
 import com.polimi.childcare.server.Helper.DBHelper;
 import com.polimi.childcare.server.database.DatabaseSession;
 import com.polimi.childcare.shared.networking.requests.filtered.FilteredBaseRequest;
