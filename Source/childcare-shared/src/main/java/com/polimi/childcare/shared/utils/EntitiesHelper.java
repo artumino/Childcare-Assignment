b8c020826fe1f3821c092cca740a18544a5fed68
artum <jacopo.libe@gmail.com>
2018-05-24 17:14:54 +0200

    Aggiunto metodo per la creazione di una mappa ricercabile delle presenze per bambino

--- Source/childcare-shared/src/main/java/com/polimi/childcare/shared/utils/EntitiesHelper.java
+++ Source/childcare-shared/src/main/java/com/polimi/childcare/shared/utils/EntitiesHelper.java
@@ -1,9 +1,18 @@
 package com.polimi.childcare.shared.utils;
 
+import com.polimi.childcare.shared.entities.Bambino;
+import com.polimi.childcare.shared.entities.RegistroPresenze;
+
 import java.util.*;
 
 public class EntitiesHelper
 {
+    /**
+     * Metodo che dato un set ritorna un clone non modificabile
+     * @param set Set di elementi da clonare
+     * @param <T> Tipo degli elementi nel set
+     * @return HashSet di elementi di tipo T non modificabile
+     */
     public static <T> Set <T> unmodifiableListReturn(Set<T> set)
     {
         if(set == null)
@@ -12,5 +21,28 @@ public class EntitiesHelper
         Set<T> list = new HashSet<>(set);
         Collections.unmodifiableSet(list);
         return list;
+    }
+
+    /**
+     * Metodo utile per convertire una lista (in generale iterable) di RegistroPresenze inizializzato in una struttura
+     * ottimizzata per la ricerca tramite Bambino
+     * @param presenze Lista registro presenze
+     * @return Lista che associa 1 RegistroPresenze ad 1 Bambino (in caso di più registri per bambini ritorna l'ultimo disponibile)
+     */
+    public static HashMap<Bambino, RegistroPresenze> presenzeToSearchMap(Iterable<RegistroPresenze> presenze)
+    {
+        HashMap<Bambino,RegistroPresenze> mappaPresenze = new HashMap<>();
+
+        for(RegistroPresenze presenza : presenze)
+        {
+            if(presenza.getBambino() != null)
+            {
+                Bambino bambino = presenza.getBambino();
+                if(!mappaPresenze.containsKey(bambino) || mappaPresenze.get(bambino).getTimeStamp().before(presenza.getTimeStamp()))
+                    mappaPresenze.put(bambino, presenza);
+            }
+        }
+
+        return mappaPresenze;
     }
 }
